a77b0cc8e423398bea63bf4fd1af9af1e1fa6425
gn-vr <1sainigan@hdsb.ca>
2024-05-03 12:03:19 -0400

    IR and Color sensor code added

--- RobotGroup10/RobotGroup10.ino
+++ RobotGroup10/RobotGroup10.ino
@@ -1,11 +1,44 @@
 // hey there is code in here!!!
+#include <tcs3200.h>
+#define COLOR_0 A0
+#define COLOR_1 A1
+#define COLOR_2 A2
+#define COLOR_3 A3
+#define COLOR_OUT A4
+
+#define IR_1 4
+#define IR_2 7
+#define IR_3 8
+#define IR_4 12
+
+unsigned long colorSensorMillis = 0;
+unsigned long irSensorMillis = 0; // Timer to track the last report of teh IR sensors
 
 void setup() {
-  // put your setup code here, to run once:
+// put your setup code here, to run once:
 
+// Setup Infrared Pins
+  pinMode(IR_1, INPUT);
+  pinMode(IR_2, INPUT);
+  pinMode(IR_3, INPUT);
+  pinMode(IR_4, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // Get the current run time in milliseconds
+  unsigned long currentMillis = millis();
+
+  // Check the states of the IR sensors every 500ms
+  if (currentMillis - irSensorMillis >=500) {
+    irSensorMillis = currentMillis;
+    readInfrared();
+  }
+
+  // Read the color sensor
+  if (currentMillis - colorSensorMillis >= 250) {
+    colorSensorMillis = currentMillis;
+    readColorSensor();
+  }
 }
